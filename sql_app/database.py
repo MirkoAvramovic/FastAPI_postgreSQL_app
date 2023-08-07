7164102aef6234608e46a68e0fcbd8b49d8a909f
MirkoAvramovic <mirkoavramovicsluzbeni@gmail.com>
2023-08-07 20:00:32 +0200

    Create & Read

--- sql_app/database.py
+++ sql_app/database.py
@@ -6,8 +6,8 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,  connect_args={"check_same_tread": False} #...is needed only for SQLite. It's not needed for other databases.
+    SQLALCHEMY_DATABASE_URL,  connect_args={"check_same_thread": False} #...is needed only for SQLite. It's not needed for other databases.
 )
-SessionLocal = sessionmaker(autocomit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
